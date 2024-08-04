fd1156e83ecaca1df42ffa765af315f4e33ac1af
Sandhyalalam <155603855+Sandhyalalam@users.noreply.github.com>
2024-08-04 17:05:19 +0530

    import java.util.Scanner; public class Main{     public static void main(String[] args){         int a,b;         Scanner read=new Scanner(System.in);         a=read.nextInt();         b=read.nextInt();         System.out.printf("%d %d",b,a);     } }

---
+++ Swap_Two_Numbers.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args){
+        int a,b;
+        Scanner read=new Scanner(System.in);
+        a=read.nextInt();
+        b=read.nextInt();
+        System.out.printf("%d
+%d",b,a);
+    }
+}
