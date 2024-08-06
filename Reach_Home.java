e9b42cb7f111cf68ad810c759baca43089af84c0
Sandhyalalam <155603855+Sandhyalalam@users.noreply.github.com>
2024-08-06 06:52:37 +0530

    import java.util.Scanner; public class Main{     public static void main(String[] args)     {         int x,y;         Scanner read=new Scanner(System.in);         x=read.nextInt();         y=read.nextInt();         if(5*x>=y)         {             System.out.println("YES");         }         else         {             System.out.println("NO");         }
        }
    }

---
+++ Reach_Home.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args)
+    {
+        int x,y;
+        Scanner read=new Scanner(System.in);
+        x=read.nextInt();
+        y=read.nextInt();
+        if(5*x>=y)
+        {
+            System.out.println("YES");
+        }
+        else
+        {
+            System.out.println("NO");
+        }
+        
+    }
+}
