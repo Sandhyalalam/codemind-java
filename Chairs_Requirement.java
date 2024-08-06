876560f2a89d3e11a068a6200507af0aa79e311c
Sandhyalalam <155603855+Sandhyalalam@users.noreply.github.com>
2024-08-06 07:54:49 +0530

    import java.util.Scanner; public class Main{     public static void main(String[] args)     {         int x,y,r;         Scanner read=new Scanner(System.in);         x=read.nextInt();         y=read.nextInt();
            r=x-y;
            {
                System.out.println(r);
            }
            
        }
    }

---
+++ Chairs_Requirement.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args)
+    {
+        int x,y,r;
+        Scanner read=new Scanner(System.in);
+        x=read.nextInt();
+        y=read.nextInt();
+       
+        r=x-y;
+        {
+            System.out.println(r);
+        }
+        
+    }
+}
