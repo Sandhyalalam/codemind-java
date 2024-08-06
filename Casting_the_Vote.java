43646c91fa17c4bb4caa7b55723a50b32497cad3
Sandhyalalam <155603855+Sandhyalalam@users.noreply.github.com>
2024-08-06 07:17:57 +0530

    import java.util.Scanner; public class Main{     public static void main(String[] args)     {         int x;         Scanner read=new Scanner(System.in);         x=read.nextInt();         if(x>=18)         {             System.out.printf("YES");         }         else         {             System.out.printf("NO");         }     } }

---
+++ Casting_the_Vote.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args)
+    {
+        int x;
+        Scanner read=new Scanner(System.in);
+        x=read.nextInt();
+        if(x>=18)
+        {
+            System.out.printf("YES");
+        }
+        else
+        {
+            System.out.printf("NO");
+        }
+    }
+}
